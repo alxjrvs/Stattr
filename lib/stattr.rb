be7ea570be438429ff52ee59aee61f142ef611b9
Alex Jarvis <alxjrvs@gmail.com>
2012-05-24 00:32:57 -0400

    all documented

--- lib/stattr.rb
+++ lib/stattr.rb
@@ -61,11 +61,29 @@ module Stattr
     end #roll
   end #DiceRoll
 
+  # Represents the list of Statistics that a player has. 
+  #
 	class StatList 
 
+    # A list of all stats. Currently locked into D+D style stats. 
+    #
 	  attr_accessor :str, :dex, :cha, :con, :wis, :int
 
-    def initialize(str=10, dex=10, cha=10, con=10, wis=10, int=10)  # :notnew:
+    # Create a new Statlist. 
+    #
+    # @param [Array] str
+    #
+    # @param [Array] dex
+    #
+    # @param [Array] cha
+    #
+    # @param [Array] con
+    #
+    # @param [Array] wis
+    #
+    # @param [Array] int
+    #
+    def initialize(str=10, dex=10, cha=10, con=10, wis=10, int=10)  
       @str = modstat(str)
       @dex = modstat(dex) 
       @cha = modstat(cha)
@@ -73,24 +91,46 @@ module Stattr
       @wis = modstat(wis)
       @int = modstat(int)
     end #initialize
-      
+    
+    # Takes a given value of a stat and creates a D+D style modifier. 
+    #
+    # @return [Array] modlist. [0] is the stat, [1] is the modifier. 
+    #
     def modstat(r)
       modr = ((r-10)/2).to_int
       modlist = [r, modr]
       modlist
     end #modstat
   end #StatList
+  
+  # CharacterSheet of a given PlayerCharacter. 
+  #
+	class CharacterSheet 
 
-	class Charactersheet 
+    # @attribute stats [Object] A StatList object 
+    #
+    # @attribute name [String] The Player Character's name 
+    #
   	attr_accessor :stats, :name
 
+    # new instance of CharacterSheet
+    #
+    # @param [String] name 
+    #
   	def initialize(name) # :notnew:
     	@name = name
+      # creates StatList object with '10' as a default for each. 
     	@stats = StatList.new(10,10,10,10,10,10)
   	end #initialize 
 		
+    # This is how you roll a brand new random character. 
+    #
+    # @param [String] name 
+    #
+    # @return [Object] New Charactersheet 
+    #
 		def self.roll_char(name)
-      char = Charactersheet.new(name) 
+      char = CharacterSheet.new(name) 
       char.stats = StatList.new(
       DiceRoll.new_roll(DICE_SIDES, 3),
       DiceRoll.new_roll(DICE_SIDES, 3),
@@ -102,18 +142,37 @@ module Stattr
 		  char	
 		end # self.roll_char
 	end #CharacterSheet
-
+  
+  # Player class represents a human, who may have multiple character sheets. 
+  #
   class Player 
-    attr_accessor :fname, :lname, :characters
+  
+  # @attribute fname [String] The First Name of the Player   
+  #
+  # @attribute lname [String] The last Name of the Player   
+  #
+  # @attribute characters [Array] An array containing Charactersheet objects
+  #
+  attr_accessor :fname, :lname, :characters
     
-    def initialize(fname, lname) # :notnew:
+    # Create new Player object
+    #
+    # @param [String] fname
+    #
+    # @param [String] lname
+    #
+    def initialize(fname, lname) 
       @fname = fname
       @lname = lname 
       @characters = []
     end #initialize
   
+    # Creates a new randomly generated character associated with Player 
+    #
+    # @param [String] name 
+    #
     def new_char(name)
-      new_char = Charactersheet.roll_char(name)
+      new_char = CharacterSheet.roll_char(name)
       self.characters << new_char 
     end #roll_char
   end #Player
